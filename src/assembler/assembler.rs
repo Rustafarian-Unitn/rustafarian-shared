e0bb64aeb8a62afa1ad08dc9fcc723c365ffb615
Lorenzo <lorenzofumi@gmail.com>
2024-12-19 20:40:13 +0100

    feat: added assembler/disassembler

---
+++ src/assembler/assembler.rs
@@ -0,0 +1,60 @@
+#![allow(unused)]
+use std::collections::{HashMap, HashSet};
+use std::{fs, thread};
+use wg_2024::config::Config;
+use wg_2024::controller::{DroneCommand, DroneEvent};
+use wg_2024::drone::Drone;
+use wg_2024::network::{NodeId, SourceRoutingHeader};
+use wg_2024::packet::{FloodRequest, FloodResponse, Fragment, NodeType};
+use wg_2024::packet::{Packet, PacketType};
+
+pub struct Assembler {
+    //map every fragment set with it's sessionID
+    received_fragment: HashMap<u64, Vec<Fragment>>,
+}
+
+impl Assembler {
+    pub fn new() -> Self {
+        Assembler {
+            received_fragment: HashMap::new(),
+        }
+    }
+
+    //adds the fragment based on the session id and returns it if the message is complete
+    pub fn add_fragment(&mut self, fragment: Fragment, session_id: u64) -> Option<Vec<u8>> {
+        //check if session id already present or create empty entry with new Vec
+        let fragments = self
+            .received_fragment
+            .entry(session_id)
+            .or_insert_with(Vec::new);
+
+        //push the fragment in the right vec
+        fragments.push(fragment);
+
+        //if the fragments lenght == field total_n_fragments => reassemble message and return
+        if let Some(total_fragment) = fragments.first().map(|f| f.total_n_fragments) {
+            if fragments.len() == total_fragment as usize {
+                return Some(self.reassemble_message(session_id));
+            }
+        }
+        None
+    }
+
+    fn reassemble_message(&mut self, session_id: u64) -> Vec<u8> {
+        //take the fragments corresponding to session id
+        if let Some(mut fragments) = self.received_fragment.remove(&session_id) {
+            //sort the fragment by index
+            fragments.sort_by_key(|f| f.fragment_index);
+
+            let mut message = Vec::new();
+            for fragment in fragments {
+                //add fragment data to message
+                message.extend_from_slice(&fragment.data[..fragment.length as usize]);
+            }
+
+            return message;
+        }
+
+        Vec::new() //to fix
+    }
+}
---
+++ src/assembler/disassembler.rs
@@ -0,0 +1,48 @@
+#![allow(unused)]
+use std::collections::{HashMap, HashSet};
+use std::{fs, thread};
+use wg_2024::config::Config;
+use wg_2024::controller::{DroneCommand, DroneEvent};
+use wg_2024::drone::Drone;
+use wg_2024::network::{NodeId, SourceRoutingHeader};
+use wg_2024::packet::{FloodRequest, FloodResponse, Fragment, NodeType};
+use wg_2024::packet::{Packet, PacketType};
+
+pub const FRAGMENT_DSIZE: usize = 128;
+
+pub struct Disassembler {}
+
+impl Disassembler {
+    //create hashmap empty
+    pub fn new() -> Self {
+        Disassembler {}
+    }
+
+    pub fn disassemble_message(&mut self, message: Vec<u8>, session_id: u64) -> Vec<Fragment> {
+        let mut fragments = Vec::<Fragment>::new();
+        //ceil rounds the decimal number to the next whole number
+        let total_fragments = (message.len() as f64 / FRAGMENT_DSIZE as f64).ceil() as u64;
+
+        // Break the message into fragments (chunks) and iter over it
+        for (i, chunk) in message.chunks(FRAGMENT_DSIZE).enumerate() {
+            //divide the message in chunks
+            let mut data = [0u8; FRAGMENT_DSIZE];
+            let length = chunk.len() as u8; // chunk length
+
+            // copy chucnk into data
+            data[..length as usize].copy_from_slice(chunk);
+
+            // create fragment and add it to list
+            let fragment = Fragment {
+                fragment_index: i as u64,
+                total_n_fragments: total_fragments,
+                length,
+                data,
+            };
+
+            fragments.push(fragment);
+        }
+
+        fragments
+    }
+}
