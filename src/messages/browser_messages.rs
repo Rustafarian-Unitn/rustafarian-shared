6be8fdad90c2ad83289c41f310651a430a9f7667
Lorenzo <lorenzofumi@gmail.com>
2024-12-20 14:52:37 +0100

    feat: added browser messages

---
+++ src/messages/browser_messages.rs
@@ -0,0 +1,48 @@
+use serde::{Deserialize, Serialize};
+use crate::messages::general_messages::{DroneSend, Request, Response, ServerTypeResponse};
+
+use super::general_messages::ServerTypeRequest;
+
+/**
+ * Request type for a chat client
+ */
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BrowserRequest {
+    FileList,
+    TextFileRequest(u8),
+    MediaFileRequest(u8),
+}
+
+impl DroneSend for BrowserRequest {}
+impl Request for BrowserRequest {}
+
+/**
+ * Response type for a chat client
+ */
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BrowserResponse {
+    FileList(Vec<u8>),
+    TextFile(String),
+    MediaFile(String),
+}
+
+impl DroneSend for BrowserResponse {}
+impl Response for BrowserResponse {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BrowserResponseWrapper {
+    Chat(BrowserResponse),
+    ServerType(ServerTypeResponse),
+}
+
+impl Response for BrowserResponseWrapper {}
+impl DroneSend for BrowserResponseWrapper {}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BrowserRequestWrapper {
+    Chat(BrowserRequest),
+    ServerType(ServerTypeRequest),
+}
+
+impl Request for BrowserRequestWrapper {}
+impl DroneSend for BrowserRequestWrapper {}
