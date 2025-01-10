77d751984763ebc3862d9f774e5e4c49d3f9ab3e
eneastrambini <strambinienea18@gmail.com>
2025-01-10 17:10:59 +0100

    feat: add logger as shared code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod assembler;
 pub mod messages;
 pub mod topology;
+pub mod logger;
 
 pub const TIMEOUT_TIMER_MS: u64 = 500;
 pub const TIMEOUT_BETWEEN_FLOODS_MS: u64 = 500;
---
+++ src/logger.rs
@@ -0,0 +1,61 @@
+use wg_2024::network::NodeId;
+
+/// Used in the log method
+/// * `INFO`: default log level, will always be printed
+/// * `DEBUG`: used only in debug situation, will not print if the debug flag is `false`
+/// * `ERROR`: will print the message to `io::stderr`
+pub enum LogLevel { INFO, DEBUG, ERROR }
+
+/// Struct used to cleanly log messages and information
+///
+/// * `node_type: String` - the type of the node logging the information, e.g. Chat Client
+/// * `node_id: NodeId` - the id of the node logging the information
+/// * `debug: bool ` - debug flag, if true then it will log DEBUG level
+pub struct Logger {
+    node_type: String,
+    node_id: NodeId,
+    debug: bool
+}
+
+impl Logger {
+
+    pub fn new(node_type: String, node_id: NodeId, debug: bool) -> Logger {
+        Logger {
+            node_type,
+            node_id,
+            debug
+        }
+    }
+
+    /// Utility method used to cleanly log information, differentiating on three different levels
+    ///
+    /// # Args
+    /// * `log_message: &str` - the message to log
+    /// * `log_level: LogLevel` - the level of the log:
+    ///     * `INFO`: default log level, will always be printed
+    ///     * `DEBUG`: used only in debug situation, will not print if the debug flag is `false`
+    ///     * `ERROR`: will print the message to `io::stderr`
+    pub fn log(
+        &self,
+        log_message: &str,
+        log_level: LogLevel,
+    ) {
+
+        match log_level {
+            LogLevel::INFO => {
+                print!("[{} {}] - LEVEL: INFO >>> ", self.node_type, self.node_id);
+                println!("{}", log_message);
+            }
+            LogLevel::DEBUG => {
+                if self.debug {
+                    print!("[{} {}] - LEVEL: DEBUG >>> ", self.node_type, self.node_id);
+                    println!("{}", log_message);
+                }
+            }
+            LogLevel::ERROR => {
+                eprint!("[{} {}] - LEVEL: ERROR >>> ",  self.node_type, self.node_id);
+                eprintln!("{}", log_message);}
+        }
+    }
+
+}
